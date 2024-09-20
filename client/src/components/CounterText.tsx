4698754df63731406fcf4aea3451be6fce4a7474
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-09-19 20:18:46 -0700

    added typography component on CounterText component

--- client/src/components/CounterText.tsx
+++ client/src/components/CounterText.tsx
@@ -1,14 +1,15 @@
 import { counter } from "@/classes/Counter";
 import { observer } from "mobx-react";
+import { Typography } from "@mui/material";
 export const CounterText = observer(() => {
   return (
     <div>
-      <h1>Counter: {counter.count}</h1>
+      <Typography variant="h3">Content {counter.count}</Typography>
       <button onClick={() => counter.incrementCounter()}>
-        Increment Counter
+        <Typography variant="button">Increment Counter</Typography>
       </button>
       <button onClick={() => counter.decrementCounter()}>
-        Decrement Counter
+        <Typography variant="button">Decrement Counter</Typography>
       </button>
     </div>
   );
