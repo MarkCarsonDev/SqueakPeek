534593f27e2bdb562ad593a01263e38f37fc9b05
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-09-24 23:02:43 -0700

    implemented navbar functionality, no styling yet

--- client/src/ui/NavBar.tsx
+++ client/src/ui/NavBar.tsx
@@ -1,3 +1,41 @@
+import { Typography } from "@mui/material";
+import Link from "next/link";
+
+interface ILinks {
+  name: string;
+  href: string;
+}
 export function NavBar() {
-  return <div>This is the navbar</div>;
+  const links: ILinks[] = [
+    {
+      name: "Explore",
+      href: "/explore",
+    },
+    {
+      name: "Message",
+      href: "/message",
+    },
+    {
+      name: "Track",
+      href: "/track",
+    },
+    {
+      name: "Profile",
+      href: "/profile",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      {links.map((link) => (
+        <Link key={link.name} href={link.href}>
+          <Typography variant="body1">{link.name}</Typography>
+        </Link>
+      ))}
+    </div>
+  );
 }
