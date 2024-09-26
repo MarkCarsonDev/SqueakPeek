fee4622180b85e67975899a4593c951e0e3a9b63
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-09-25 21:35:05 -0700

    added test change to test vercel PR

--- client/src/ui/CounterText.tsx
+++ client/src/ui/CounterText.tsx
@@ -6,6 +6,7 @@ export function CounterText() {
   const [count, setCount] = useState<number>(0);
   return (
     <div>
+      <Typography variant="h1">I love avocadoes</Typography>
       <Typography variant="h3">Counter: {count}</Typography>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         <Typography variant="button">Increment Counter</Typography>
