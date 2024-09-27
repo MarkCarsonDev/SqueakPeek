e6474c68601ca3e6d6dba8d75d0f29d39d02b301
Hao Phan <rinnphan01@gmail.com>
2024-09-27 11:57:35 -0700

    Upload the X logo folder and add in the X icon

--- client/src/ui/PageFooter.tsx
+++ client/src/ui/PageFooter.tsx
@@ -7,28 +7,34 @@ const PageFooter = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                {/* Media Part */}
-                <div className="footer-column">
-                    <h4>Social Media</h4>
-                    <ul>
-                        <li><Link href ="#">Meta</Link></li>
-                        <li><Link href ="#">Instagram</Link></li>
-                        <li><Link href ="#">X</Link></li>
-                        <li><Link href ="#">LinkedIn</Link></li>
-                    </ul>
+                {/* Left Section */}
+                <div className="footer-section">
+                    <div className="footer-links">
+                        <Link href="#" className="footer-link">Explore</Link>
+                        <Link href="#" className="footer-link">About Us</Link>
+                        <Link href="#" className="footer-link">Contact</Link>
+                    </div>
                 </div>
-                {/* About the Company part */}
-                <div className="footer-column">
-                    <h4>About</h4>
-                    <ul>
-                        <li><Link href ="#">Company</Link></li>
-                        <li><Link href ="#">Team</Link></li>
-                        {/* <li>History</li> */}
-                    </ul>
+
+                {/* Center Section */}
+                <div className="footer-section footer-center">
+                    <div className="footer-divider"></div>
+                    <h4 className="footer-logo">SqueakPeek</h4>
+                    <div className="footer-divider"></div>
+                </div>
+
+                {/* Right Section */}
+                <div className="footer-section footer-icons">
+                    <Image src="/x-logo/X-logo-black.png" alt="X" width={24} height={24} />
+                    <Image src="/instagram-icon.svg" alt="Instagram" width={24} height={24} />
+                    <Image src="/linkedin-icon.svg" alt="LinkedIn" width={24} height={24} />
+                    <Image src="/meta-icon.svg" alt="Meta" width={24} height={24} />
                 </div>
             </div>
+
+            {/* Copyright Section */}
             <div className="footer-bottom">
-                <p>© 2024 SQUEAKPEAK</p>
+                <p>© 2024 SQUEAKPEEK</p>
             </div>
         </footer>
     );
