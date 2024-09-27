efaa1ea9f210b52223f9dc75613894474d910a26
Hao Phan <rinnphan01@gmail.com>
2024-09-27 12:40:32 -0700

    I will comeback and fix the logo for Linkedin and Meta

--- client/src/ui/PageFooter.tsx
+++ client/src/ui/PageFooter.tsx
@@ -8,7 +8,7 @@ const PageFooter = () => {
         <footer className="footer">
             <div className="footer-container">
                 {/* Left Section */}
-                <div className="footer-section">
+                <div className="footer-section left">
                     <div className="footer-links">
                         <Link href="#" className="footer-link">Explore</Link>
                         <Link href="#" className="footer-link">About Us</Link>
@@ -17,18 +17,20 @@ const PageFooter = () => {
                 </div>
 
                 {/* Center Section */}
-                <div className="footer-section footer-center">
+                <div className="footer-section center">
                     <div className="footer-divider"></div>
                     <h4 className="footer-logo">SqueakPeek</h4>
                     <div className="footer-divider"></div>
                 </div>
 
                 {/* Right Section */}
-                <div className="footer-section footer-icons">
-                    <Image src="/x-logo/X-logo-black.png" alt="X" width={24} height={24} />
-                    <Image src="/instagram-icon.svg" alt="Instagram" width={24} height={24} />
-                    <Image src="/linkedin-icon.svg" alt="LinkedIn" width={24} height={24} />
-                    <Image src="/meta-icon.svg" alt="Meta" width={24} height={24} />
+                <div className="footer-section right">
+                    <div className="footer-icons">
+                        <Image src="/x-logo/X-logo-black.png" alt="X" width={24} height={24} />
+                        <Image src="/Instagram-logo/Instagram_Glyph_Black.svg" alt="Instagram" width={24} height={24} />
+                        <Image src="/linkedin-icon.svg" alt="LinkedIn" width={24} height={24} />
+                        <Image src="/Meta-logo/Meta.png" alt="Meta" width={24} height={24} />
+                    </div>
                 </div>
             </div>
 
