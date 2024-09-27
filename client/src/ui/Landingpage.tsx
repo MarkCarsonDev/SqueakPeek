a4cd3dbf9e01fd4a54ba255b94a69f8160f6c597
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-09-27 15:53:48 -0700

    added comments for readability

--- client/src/ui/Landingpage.tsx
+++ client/src/ui/Landingpage.tsx
@@ -4,14 +4,17 @@ import Image from "next/image";
 import TextCarousel from "./lpTextCarousel";
 import { Typography } from "@mui/material";
 import Link from '@mui/material/Link';
+import { red } from "@mui/material/colors";
 
 
 
 function Home(){
     return(
         <div>
+            {/*Section 1 Block*/}
             <div className="sec1-container">
                 <div className="sec1-box">
+
                     <div className="sec1-text-wheel">
                         <TextCarousel />
                     </div>
@@ -41,6 +44,7 @@ function Home(){
                 </div>
             </div>
 
+            {/*Section 2 Block*/}
             <div className="sec2-container">
                 <Image className="sec2-image"
                     src="/rat_2.png"
@@ -60,6 +64,7 @@ function Home(){
                 </div>
             </div>
 
+            {/*Section 3 Block*/}
             <div className="sec3-container">
                 <Image className="sec3-image"
                     src="/rat_1.png"
@@ -78,7 +83,7 @@ function Home(){
                 </div>
 
             </div>
-
+            {/*Section 4 Block*/}
             <div className="sec4-container">
                 <Image className="sec4-image"
                     src="/rat_3.png"
@@ -87,7 +92,9 @@ function Home(){
                     alt="Rat looking at star"
                 />
                 <div className="sec4-text-box">
-                    <Typography variant="h3">
+                    <Typography variant="h3" sx={{
+                        color: red,
+                    }}>
                         Gain Insight
                     </Typography>
 
