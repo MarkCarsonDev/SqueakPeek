0888e9b0c0fb7f2d8681c7d50470e33e0d42cb1f
Hao Phan <rinnphan01@gmail.com>
2024-10-01 16:04:05 -0700

    Add comments

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,9 @@
-import { CounterText } from "@/ui/CounterText";
+
 import { LandingNavbar } from "@/ui/LandingNavBar";
+
+/*
+This will represpent the home page of the application
+* */
 export default function Home() {
   return <LandingNavbar />;
 }
