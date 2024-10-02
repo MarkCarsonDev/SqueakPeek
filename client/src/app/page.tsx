d3513e025da05122b159277c151270e1ab0b3842
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-10-02 11:29:36 -0700

    updated with image restructure

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,6 +7,39 @@ import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
 
 export default function Home() {
+
+    {/*Added Image Array to allow for easy image changes*/}
+    const ImageData = [
+      {
+        // Section 1 Logo Image
+        imageSrc: "/LandingPage-images/rat_1.png",
+        imageAlt: "SqueakPeek Logo",
+        imageWidth: 300,
+        imageHeight: 300,
+      },
+      {
+        // Section 2 Threads Feature
+        imageSrc: "/LandingPage-images/rat_2.png",
+        imageAlt: "Two rats speaking",
+        imageWidth: 600,
+        imageHeight: 300,
+      },
+      {
+        // Section 3 Tracking Feature
+        imageSrc: "/LandingPage-images/rat_1.png",
+        imageAlt: "Rat looking up",
+        imageWidth: 400,
+        imageHeight: 400,
+      },
+      {
+        // Section 4 Explore Feature
+        imageSrc: "/LandingPage-images/rat_3.png",
+        imageAlt: "Rat looking at star",
+        imageWidth: 400,
+        imageHeight: 400,
+      },
+    ];
+
   return (
     <div>
       <LandingNavbar />
@@ -20,10 +53,10 @@ export default function Home() {
           <div className="sec1-logo-box">
             <Image
               className="sec1-main-logo"
-              src="/LandingPage-images/rat_1.png"
-              width={300}
-              height={300}
-              alt="SqueakPeek Logo"
+              src={ImageData[0].imageSrc}
+              width={ImageData[0].imageWidth}
+              height={ImageData[0].imageHeight}
+              alt={ImageData[0].imageAlt}
             />
             <Typography variant="h2">SqueakPeek</Typography>
             <Typography className="sec1-logo-text" variant="h4">
@@ -39,6 +72,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      
     </div>
   );
 }
