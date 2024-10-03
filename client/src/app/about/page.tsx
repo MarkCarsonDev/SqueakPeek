646110d931fb71b471ee6e69bfb054b41ce144d9
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-03 10:24:16 -0700

    added meta data for the AboutProfile components

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -7,9 +7,49 @@ export default function Page() {
       name: "Mekhi Hart Dela Cruz",
       role: "Lead",
       description:
-        "Mekhi Hart has vast experience developing software from video games, mobile, and web. He also like to rock climb in his spare time.",
+        "Mekhi Hart is a Software Engineer at Netflix and has vast experience developing software from video games, mobile, and web. He also like to rock climb in his spare time.",
       linkedInURl: "https://www.linkedin.com/in/mekhihart-delacruz/",
       profilePic: "/about/profilepic_mekhi.png",
+    },
+    {
+      name: "Mark Carson",
+      role: "Full-Stack",
+      description:
+        "Mark Carson has a diverse background in full-stack development and machine learning applications. He spends time AFK playing piano and in the kitchen.",
+      linkedInURl: "https://www.linkedin.com/in/markcarsondev/",
+      profilePic: "/about/profilepic_mark.png",
+    },
+    {
+      name: "David Serrano",
+      role: "Frontend",
+      description:
+        "David Serrano is an aspiring software developer and or IT technician with a passion for fitness and a constant drive to learn and grow.",
+      linkedInURl: "#", // TODO Add missing url
+      profilePic: "/about/profilepic_mark.png", // TODO Add missing profile pic
+    },
+    {
+      name: "Kate Steer",
+      role: "Backend",
+      description:
+        "Kate Steer is a software developer with experience in database management and architecture, well as application design. In her free time, she enjoys creating music and reading.",
+      linkedInURl: "https://www.linkedin.com/in/kate-steer-4149001a6/",
+      profilePic: "/about/profilepic_kate.png",
+    },
+    {
+      name: "Howie",
+      role: "Frontend",
+      description:
+        "Hao Phan is a full stack developer with experience in IT and automated flow, with passion in web development. In his free time, he likes watching soccer and formula 1.",
+      linkedInURl: "https://www.linkedin.com/in/hao-phan-b049b91aa/",
+      profilePic: "/about/profilepic_howie.png",
+    },
+    {
+      name: "Noah Daniels",
+      role: "Backend",
+      description:
+        "Noah Daniels is a software developer, data analyst, and machine learning engineer, using his passion for mathematics and music alongside a strong work ethic to achieve his aims in a broad range of sciences and life goals.",
+      linkedInURl: "https://www.linkedin.com/in/noah-daniels-07b5401a9/",
+      profilePic: "/about/profilepic_mark.png", // TODO: Add missing profile pic
     },
   ];
   return (
