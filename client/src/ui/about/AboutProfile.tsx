0b604c52b5c9c66ba1f1d8f043f594e48af45228
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-02 23:25:21 -0700

    created header of AboutProfile component

--- client/src/ui/about/AboutProfile.tsx
+++ client/src/ui/about/AboutProfile.tsx
@@ -1,3 +1,6 @@
+import { Card, CardHeader, Typography } from "@mui/material";
+import mekhiPic from "@/../../public/about/profilepic_mekhi.png";
+import Image from "next/image";
 export interface IAboutProfile {
   name: string;
   role: string;
@@ -6,5 +9,32 @@ export interface IAboutProfile {
   profilePic: string;
 }
 export function AboutProfile() {
-  return <div className="borderline">Hello</div>;
+  return (
+    <Card
+      className="borderline"
+      sx={{
+        maxWidth: 345,
+      }}
+    >
+      <CardHeader
+        avatar={
+          <Image
+            style={{
+              borderRadius: 12,
+            }}
+            width={75}
+            height={75}
+            src={mekhiPic}
+            alt=""
+          />
+        }
+        title={
+          <Typography sx={{ fontWeight: "bold" }} variant="body1">
+            Mekhi Hart Dela Cruz
+          </Typography>
+        }
+        subheader={<Typography variant="subtitle1">Lead</Typography>}
+      ></CardHeader>
+    </Card>
+  );
 }
