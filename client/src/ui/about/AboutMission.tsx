e3356927f5881e5ea47cde82f2f74efcce615876
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-03 15:49:31 -0700

    modified padding for AboutMission component

--- client/src/ui/about/AboutMission.tsx
+++ client/src/ui/about/AboutMission.tsx
@@ -4,7 +4,7 @@ export function AboutMission() {
     <div
       style={{
         backgroundColor: "#E0E4F2",
-        padding: "50px 0px",
+        padding: "50px",
         marginBottom: "30px",
       }}
     >
@@ -13,11 +13,15 @@ export function AboutMission() {
           textAlign: "center",
           marginBottom: "20px",
         }}
-        variant="h4"
+        variant="h5"
       >
-        We are a dedicated team of Computer Science students from California
-        State University Long Beach, who understand firsthand the challenges of
-        job searching and building professional connections.
+        We are a dedicated team of college students who understand firsthand the
+        challenges of job searching and building professional connections.
+        That’s why, in 2024, we began SqueakPeek to allow you to connect with
+        other job applicants, share experiences, and track your progress every
+        step of the way. Through message threads, you can communicate about
+        opportunities, discuss interview insights, and feel more informed as you
+        move forward in your career.
       </Typography>
       <Typography
         sx={{
