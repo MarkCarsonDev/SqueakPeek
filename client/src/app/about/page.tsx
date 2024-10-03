b001150df1362233892ae620355b59fa39f4ee1e
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-03 10:53:54 -0700

    added key prop to AboutProfile component

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -101,7 +101,7 @@ export default function Page() {
               margin: "20px",
             }}
           >
-            <AboutProfile {...profile} />
+            <AboutProfile key={profile.name} {...profile} />
           </div>
         ))}
       </div>
