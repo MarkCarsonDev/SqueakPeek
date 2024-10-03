9317b4720ccf34eced9e779b617cd9cc13e3294a
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-02 21:58:36 -0700

    Created text content for the About page

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,8 +1,42 @@
 import { AboutHero } from "@/ui/about/AboutHero";
+import { Typography } from "@mui/material";
 export default function Page() {
   return (
-    <div>
+    <div style={{}}>
       <AboutHero />
+
+      <Typography
+        sx={{
+          textAlign: "center",
+          marginTop: "50px",
+          marginBottom: "20px",
+        }}
+        variant="h4"
+      >
+        We are a dedicated team of Computer Science students from California
+        State University Long Beach, who understand firsthand the challenges of
+        job searching and building professional connections.
+      </Typography>
+      <Typography
+        sx={{
+          textAlign: "center",
+          textDecoration: "underline",
+          fontWeight: "bold",
+          marginBottom: "20px",
+        }}
+        variant="h4"
+      >
+        Our mission is to empower your job search. 
+      </Typography>
+      <Typography
+        sx={{
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
+        variant="h5"
+      >
+        The Team
+      </Typography>
     </div>
   );
 }
