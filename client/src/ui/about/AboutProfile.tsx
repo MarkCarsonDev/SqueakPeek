8be5edc792196f699bbc6d303aa8d8ede692d460
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-02 21:32:31 -0700

    set up About page, and AboutProfile component

---
+++ client/src/ui/about/AboutProfile.tsx
@@ -0,0 +1,10 @@
+export interface IAboutProfile {
+  name: string;
+  role: string;
+  description: string;
+  linkedInURl: string;
+  profilePic: string;
+}
+export function AboutProfile() {
+  return <div></div>;
+}
