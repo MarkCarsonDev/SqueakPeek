173de1f3e1284d098db669785bf93de029379782
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-04 15:34:39 -0700

    added noah's about profile pic

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -50,7 +50,7 @@ export default function Page() {
       description:
         "Noah Daniels is a software developer, data analyst, and machine learning engineer, using his passion for mathematics and music alongside a strong work ethic to achieve his aims in a broad range of sciences and life goals.",
       linkedInURl: "https://www.linkedin.com/in/noah-daniels-07b5401a9/",
-      profilePic: "/about/profilepic_mark.png", // TODO: Add missing profile pic
+      profilePic: "/about/profilepic_noah.png",
     },
   ];
   return (
