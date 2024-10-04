556800a470355717b93a530261bfcb366ef97998
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-04 16:03:49 -0700

    updated docs for layout file and  for (onboarding) directory page

--- client/src/app/(onboarding)/layout.tsx
+++ client/src/app/(onboarding)/layout.tsx
@@ -1,8 +1,8 @@
 import { LandingNavbar } from "@/ui/LandingNavBar";
 
 /**
- * This layout file affects files in the (main) directory
- * The navigation bar is set to render in each page under the (main directory)
+ * This layout file affects files in the (onboarding) directory
+ * The Landing navigation bar is set to render in each page under the (onboarding) directory
  */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- client/src/app/(onboarding)/page.tsx
+++ client/src/app/(onboarding)/page.tsx
@@ -1,8 +1,7 @@
-import { LandingNavbar } from "@/ui/LandingNavBar";
+/**
+ * This will represpent the home page of the application
+ */
 
-/*
-This will represpent the home page of the application
-* */
 export default function Home() {
   return <h1>Home Placeholder</h1>;
 }
