243876003660706d0a67420485fec136a66ecc43
Serranoda <Serranoda21@gmail.com>
2024-10-06 14:15:00 -0700

    adjusted styling  to use flexbox

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -48,16 +48,14 @@ export default function Home() {
       {/*Section 1 Block*/}
       <div className="sec1-container">
         <div className="sec1-box">
-          
           <div className="sec1-primary">
-
             <div className="sec1-text-wheel">
               <Typography variant="h4">
                 This is a placeholder for the carousel text.
               </Typography>
               {/* <TextCarousel className="sec1-text-wheel" /> Cant be referenced yet */}
             </div>
-  
+
             <div className="sec1-logo-box">
               <Image
                 className="sec1-main-logo"
@@ -67,15 +65,11 @@ export default function Home() {
                 alt={ImageData[0].imageAlt}
               />
               <Typography variant="h2">SqueakPeek</Typography>
-              <Typography variant="h4">
-                Job hunting is hard, 
-              </Typography>
-              <Typography variant="h4">
-                we’re here to help.
-              </Typography>
+              <Typography variant="h4">Job hunting is hard,</Typography>
+              <Typography variant="h4">we’re here to help.</Typography>
             </div>
           </div>
-        
+
           <div className="sec1-Link">
             <Link href="/signup">
               <Typography variant="h5" sx={{ color: "#496FFF" }}>
@@ -83,7 +77,26 @@ export default function Home() {
               </Typography>
             </Link>
           </div>
+        </div>
+      </div>
 
+      {/*Section 2 Block*/}
+      <div className="sec2-container">
+        <Image
+          className="sec2-image"
+          src={ImageData[1].imageSrc}
+          width={ImageData[1].imageWidth}
+          height={ImageData[1].imageHeight}
+          alt={ImageData[1].imageAlt}
+        />
+
+        <div className="sec2-text-box">
+          <Typography variant="h3">Talk to other applicants</Typography>
+
+          <Typography variant="body1">
+            Engage with company thread to ask questions and support fellow
+            applicants
+          </Typography>
         </div>
       </div>
     </div>
