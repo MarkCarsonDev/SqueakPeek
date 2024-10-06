763675e5a8ad387f8f96a745a931f8f292107ed5
Serranoda <Serranoda21@gmail.com>
2024-10-06 15:36:15 -0700

    added text carousel

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@
 //import { LandingNavbar } from "@/ui/LandingNavbar";
 import "./LandingPage.css";
 import Image from "next/image";
-//import { TextCarousel } from "@/ui/TextCarousel";
+import { TextCarousel } from "@/ui/TextCarousel";
 import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
 
@@ -50,10 +50,7 @@ export default function Home() {
         <div className="sec1-box">
           <div className="sec1-primary">
             <div className="sec1-text-wheel">
-              <Typography variant="h4">
-                This is a placeholder for the carousel text.
-              </Typography>
-              {/* <TextCarousel className="sec1-text-wheel" /> Cant be referenced yet */}
+              <TextCarousel />
             </div>
 
             <div className="sec1-logo-box">
