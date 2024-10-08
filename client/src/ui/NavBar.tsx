4bd19747757cca68da51994265cc674412402db0
Serranoda <Serranoda21@gmail.com>
2024-10-08 10:13:02 -0700

    navbar complete

--- client/src/ui/NavBar.tsx
+++ client/src/ui/NavBar.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { usePathname } from "next/navigation";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine } from "@fortawesome/free-solid-svg-icons";
+import { faUser} from "@fortawesome/free-solid-svg-icons";
 
 export function NavBar() {
   const pathName = usePathname();
@@ -14,150 +17,145 @@ export function NavBar() {
       style={{
         background: "white",
         height: "80px",
-        width: "100%",
         display: "flex",
         zIndex: 1,
         position: "sticky",
         borderBottom: "5px solid #E0E4F2",
+        alignContent: "center"
       }}
     >
-      <div
+      <Link
+        href="/"
         style={{
-          height: "80px",
-          width: "100%",
+          color: "#3C435C",
           display: "flex",
           alignItems: "center",
-          justifyContent: "space-between",
+          textDecoration: "none",
+          marginLeft: "1%"
         }}
       >
-        <Link
-          href="/"
+          <Image
+            src="/LandingPage-images/rat_1.png"
+            height={100}
+            width={100}
+            alt="Squeakpeek Logo"
+            style={{ objectFit: 'cover', objectPosition: 'bottom', marginBottom: "5%", marginRight: "2%"}}
+          />
+        <Typography variant="h4">SqueakPeek</Typography>
+      </Link>
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ul
           style={{
-            height: "80px",
-            color: "#3C435C",
-            position: "relative",
             display: "flex",
-            alignItems: "center",
-            textDecoration: "none",
-            left: "1%",
+            gridTemplateColumns: "repeat(4, auto)",
+            gridGap: "30px",
+            listStyle: "none",
+            textAlign: "center",
           }}
         >
-          <div
-            style={{
-              height: "100px",
-              width: "100px",
-            }}
-          >
-            <Image
-              src="/LandingPage-images/rat_1.png"
-              height={100}
-              width={100}
-              alt="Squeakpeek Logo"
+          <li>
+            <Link
+              href="/explore"
               style={{
-                position: "relative",
-                bottom: "10%",
-                right: "15%",
+                color: "#3C435C",
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
               }}
-            />
-          </div>
-          <Typography variant="h4">SqueakPeek</Typography>
-        </Link>
-
-        <div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            position: "relative",
-            height: "80px",
-            width: "500px",
-          }}
-        >
-          <ul
-            style={{
-              display: "flex",
-              gridTemplateColumns: "repeat(4, auto)",
-              gridGap: "30px",
-              listStyle: "none",
-              textAlign: "center",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <li>
-              <Link
-                href="/explore"
-                style={{
-                  color: "#3C435C",
-                  textDecoration: "none",
+            >
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                style={{ marginRight: "10%" }}
+              />
+              <Typography
+                sx={{
+                  fontWeight: pathName === "/explore" ? "bold" : "normal",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontWeight: pathName === "/explore" ? "bold" : "normal",
-                    display: "flex"
-                  }}
-                >
-                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{marginRight: "1%"}}/>Explore
-                </Typography>
-              </Link>
-            </li>
+                Explore
+              </Typography>
+            </Link>
+          </li>
 
-            <li>
-              <Link
-                href="/message"
-                style={{
-                  color: "#3C435C",
-                  textDecoration: "none",
+          <li>
+            <Link
+              href="/message"
+              style={{
+                color: "#3C435C",
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faMessage}
+                style={{ marginRight: "10%" }}
+              />
+              <Typography
+                sx={{
+                  fontWeight: pathName === "/message" ? "bold" : "normal",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontWeight: pathName === "/message" ? "bold" : "normal",
-                  }}
-                >
-                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{marginRight: "1%"}}/>Message
-                </Typography>
-              </Link>
-            </li>
+                Message
+              </Typography>
+            </Link>
+          </li>
 
-            <li>
-              <Link
-                href="/thread"
-                style={{
-                  color: "#3C435C",
-                  textDecoration: "none",
+          <li>
+            <Link
+              href="/track"
+              style={{
+                color: "#3C435C",
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faChartLine}
+                style={{ marginRight: "10%" }}
+              />
+              <Typography
+                sx={{
+                  fontWeight: pathName === "/track" ? "bold" : "normal",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontWeight: pathName === "/thread" ? "bold" : "normal",
-                  }}
-                >
-                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{marginRight: "1%"}}/>Thread
-                </Typography>
-              </Link>
-            </li>
+                Track
+              </Typography>
+            </Link>
+          </li>
 
-            <li>
-              <Link
-                href="/track"
-                style={{
-                  color: "#3C435C",
-                  textDecoration: "none",
+          <li>
+            <Link
+              href="/profile"
+              style={{
+                color: "#3C435C",
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faUser}
+                style={{ marginRight: "10%" }}
+              />
+              <Typography
+                sx={{
+                  fontWeight: pathName === "/profile" ? "bold" : "normal",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontWeight: pathName === "/track" ? "bold" : "normal",
-                  }}
-                >
-                  <FontAwesomeIcon icon={faMagnifyingGlass} style={{marginRight: "1%"}}/>Tracking
-                </Typography>
-              </Link>
-            </li>
-
-          </ul>
-        </div>
+                Profile
+              </Typography>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
