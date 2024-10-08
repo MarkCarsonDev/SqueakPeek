9ba764fa0d111aff62028648889c899e426ef01c
Serranoda <Serranoda21@gmail.com>
2024-10-07 22:41:24 -0700

    file move

--- client/src/app/(onboarding)/page.tsx
+++ client/src/app/(onboarding)/page.tsx
@@ -1,7 +1,10 @@
-/**
- * This will represpent the home page of the application
- */
+import "./LandingPage.css";
+import { LandingPageHero } from "@/ui/landingpage/LandingPageHero";
 
-export default function Home() {
-  return <h1>Home Placeholder</h1>;
+export default function Land() {
+  return (
+    <div>
+      <LandingPageHero />
+    </div>
+  );
 }
