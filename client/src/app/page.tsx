c759e795f9dc26760d63b965ab8836a594829945
Serranoda <Serranoda21@gmail.com>
2024-10-07 20:01:55 -0700

    minor edit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,10 @@
-//import { LandingNavbar } from "@/ui/LandingNavbar";
 import "./LandingPage.css";
-import Image from "next/image";
-import { Typography } from "@mui/material";
 import { LandingPageHero } from "@/ui/landingpage/LandingPageHero";
 
 export default function Home() {
   return (
     <div>
-      {/* <LandingNavbar /> Cant be referrenced yet*/}
-    <LandingPageHero />
+      <LandingPageHero />
     </div>
   );
 }
