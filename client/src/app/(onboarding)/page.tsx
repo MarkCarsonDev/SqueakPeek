39cb61ba986675b9eedef36003f229d5bd9947ab
Serranoda <Serranoda21@gmail.com>
2024-10-08 13:00:05 -0700

    size change and div removal

--- client/src/app/(onboarding)/page.tsx
+++ client/src/app/(onboarding)/page.tsx
@@ -9,38 +9,72 @@ export default function Home() {
       <LandingPageHero />
       {/*Section 2 Block*/}
       <div className="sec2-container">
-      <div className="sec2-text-box">
-          <Typography variant="h3">Talk to other applicants</Typography>
+        <div className="sec2-text-box">
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              textAlign: "center",
+              width: "700px",
+            }}
+          >
+            Talk to other applicants
+          </Typography>
 
-          <Typography variant="body1">
+          <Typography
+            variant="h4"
+            style={{
+              textAlign: "center",
+              width: "600px",
+            }}
+          >
             Engage with company thread to ask questions and support fellow
             applicants
           </Typography>
         </div>
-        <div className="sec2-image">
         <Image
           src="/LandingPage-images/rat_2.png"
-          width={600}
-          height={300}
+          width={800}
+          height={500}
           alt="Two rats speaking"
+          style={{
+            marginRight: "8%",
+          }}
         />
-        </div>
       </div>
 
       {/*Section 3 Block*/}
       <div className="sec3-container">
-        <div className="sec3-image">
         <Image
           src="/LandingPage-images/rat_1.png"
-          width={400}
+          width={500}
           height={400}
           alt="Rat looking up"
+          style={{
+            marginLeft: "8%",
+            objectFit: "cover",
+            objectPosition: "bottom",
+          }}
         />
-        </div>
         <div className="sec3-text-box">
-          <Typography variant="h3">Track your applications</Typography>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              textAlign: "center",
+              width: "700px",
+            }}
+          >
+            Track your applications
+          </Typography>
 
-          <Typography variant="body1">
+          <Typography
+            variant="h4"
+            style={{
+              textAlign: "center",
+              width: "600px",
+            }}
+          >
             Streamline your job search with ease using our tracking board
           </Typography>
         </div>
@@ -49,20 +83,38 @@ export default function Home() {
       {/*Section 4 Block*/}
       <div className="sec4-container">
         <div className="sec4-text-box">
-          <Typography variant="h3">Gain Insight</Typography>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              textAlign: "center",
+              width: "700px",
+            }}
+          >
+            Gain Insight
+          </Typography>
 
-          <Typography variant="body1">
+          <Typography
+            variant="h4"
+            style={{
+              textAlign: "center",
+              width: "600px",
+            }}
+          >
             View helpful stats about job openings with a single glance.
           </Typography>
         </div>
-        <div className="sec4-image">
-          <Image
-            src="/LandingPage-images/rat_3.png"
-            width={400}
-            height={400}
-            alt="Rat looking at star"
-          />
-        </div>
+        <Image
+          src="/LandingPage-images/rat_3.png"
+          width={600}
+          height={525}
+          alt="Rat looking at star"
+          style={{
+            marginRight: "10%",
+            objectFit: "cover",
+            objectPosition: "bottom",
+          }}
+        />
       </div>
     </div>
   );
