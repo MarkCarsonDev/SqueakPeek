dee545260c6a14ba65599fd97117212dd39d02e1
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-09 12:21:43 -0700

    created custom InputField

---
+++ client/src/ui/InputField.tsx
@@ -0,0 +1,40 @@
+import { TextFieldProps, TextField, Typography } from "@mui/material";
+
+/**
+ * A custom version of the TextField component with the label design differently
+ * @param props TextFieldProps
+ */
+export function InputField(props: TextFieldProps) {
+  const { required, label } = props;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <Typography
+          sx={{
+            textAlign: "start",
+            fontWeight: "bold",
+            display: "flex",
+          }}
+        >
+          {label}
+        </Typography>
+        {required && (
+          <Typography
+            sx={{
+              paddingLeft: "5px",
+              color: "red",
+            }}
+          >
+            *
+          </Typography>
+        )}
+      </div>
+
+      <TextField {...props} label="" />
+    </div>
+  );
+}
