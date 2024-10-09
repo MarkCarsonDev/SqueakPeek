cfca03da8bfef94b4ebd6ba4fdd734f3f9698ab0
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-09 07:01:10 -0700

    commented middleware logic to always redirect to login. Will need to change this later

--- client/lib/supabase/middleware.ts
+++ client/lib/supabase/middleware.ts
@@ -43,9 +43,11 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
+
+    // TODO: Commented for now since it's automatically redirecting to login page
+    // const url = request.nextUrl.clone();
+    // url.pathname = "/login";
+    // return NextResponse.redirect(url);
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
