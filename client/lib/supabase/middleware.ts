71bcba85461c1835577b1f1fabb0d1d2b87edb27
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-08 22:10:14 -0700

    add return statement and removed TODO in middleware file under the supabase directory

--- client/lib/supabase/middleware.ts
+++ client/lib/supabase/middleware.ts
@@ -6,7 +6,6 @@ export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
-
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
