cf11e0fd06ac674d7a66a548c0e8d1599167d5cd
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-12 20:09:03 -0700

    added zod in createAccount function, need to add supabase logic to create account

--- client/lib/actions/signup.ts
+++ client/lib/actions/signup.ts
@@ -1,4 +1,29 @@
 "use server";
+import { z } from "zod";
+const SignUpFormSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+  confirmPassword: z.string(),
+});
+
 export function createAccount(formData: FormData) {
-    
+  //TODO add form validation here. Assuming the forms are valid for now
+  const validatedFields = SignUpFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
+
+  // form validation fails
+  if (!validatedFields.success) {
+    // add error meesages
+    return {
+      errors: "", // errors for each field
+      message: "", // one message describing the failutre
+    };
+  }
+
+  const { email, password, confirmPassword } = validatedFields.data;
+
+  // call supabase to create account
 }
