5a8cd60224316216c49ebe23faead4597fee8845
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-10-14 14:01:37 -0700

    structure update

--- client/src/app/(main)/explore/page.tsx
+++ client/src/app/(main)/explore/page.tsx
@@ -1,11 +1,22 @@
-import { OpportunityCard } from "@/ui/explore/OpportunityCard"
+import { OpportunityCard } from "@/ui/explore/OpportunityCard";
+import { Typography } from "@mui/material";
+import {Card} from "@mui/material";
+import "@/app/(main)/explore/explore.css";
 
 export default function Page() {
   return (
+    <>
+      <div className="header-search-container">
 
+        
+      </div>
+      <div className="card-filter-container">
+        <div className="opportunity-column">
+          <OpportunityCard />
+        </div>
 
-    <OpportunityCard/>
-
-  )
-  
+        <Typography>Hello</Typography>
+      </div>
+    </>
+  );
 }
