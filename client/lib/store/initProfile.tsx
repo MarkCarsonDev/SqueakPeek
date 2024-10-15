df69abdc6db2e75f057985170ecb96e2c62963ed
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-14 18:31:08 -0700

    added fix to fetching profile

--- client/lib/store/initProfile.tsx
+++ client/lib/store/initProfile.tsx
@@ -2,6 +2,7 @@
 import { useProfile } from "./profile";
 import { useEffect } from "react";
 import { User } from "@supabase/supabase-js";
+import { createSupabaseClient } from "../supabase/client";
 
 /**
  * Used as a profile store to be accessible throughout the pages
@@ -9,19 +10,22 @@ import { User } from "@supabase/supabase-js";
  */
 export default function InitProfile({ user }: { user: User | null }) {
   const { profile, initializeProfile } = useProfile((state) => state);
-
   useEffect(() => {
     // only calls when an authenticated user doesn't have their profile initialized
     if (user && profile === null) {
-      // TODO: Switch this to the actual profile received from the actual user
-      initializeProfile({
-        avatar: "avatar_2",
-        created_at: new Date().toISOString(),
-        school: "CSULB",
-        username: "Bropharah",
-        user_id: "",
-        profile_id: "",
-      });
+      const supabase = createSupabaseClient();
+      supabase
+        .from("profile")
+        .select("*")
+        .eq("user_id", user.id)
+        .single()
+        .then((res) => {
+          const { data, error } = res;
+
+          if (!error && data) {
+            initializeProfile(data);
+          }
+        });
     }
   }, [profile, initializeProfile, user]);
   return null;
