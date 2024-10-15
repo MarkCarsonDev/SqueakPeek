14a57189f0005b0f2b53e0c2948934cab4d8d64b
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-15 09:57:24 -0700

    implemnted MessageInput component

--- client/src/ui/messaging/MessageInput.tsx
+++ client/src/ui/messaging/MessageInput.tsx
@@ -1,5 +1,27 @@
-import { TextField } from "@mui/material";
-
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUp } from "@fortawesome/free-solid-svg-icons/faCircleUp";
 export function MessageInput() {
-  return <TextField fullWidth />;
+  return (
+    <TextField
+      placeholder="Message..."
+      fullWidth
+      slotProps={{
+        input: {
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton>
+                <FontAwesomeIcon
+                  style={{
+                    color: "#496FFF",
+                  }}
+                  icon={faCircleUp}
+                />
+              </IconButton>
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
+  );
 }
