b867d7a7a413488963c232b5d158a7b62b1a95c1
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-14 21:01:23 -0700

    created CurrentProfileAvatar based on ProfileAvatar component

--- client/src/ui/ProfileAvatar.tsx
+++ client/src/ui/ProfileAvatar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Avatar } from "@mui/material";
 
-type AvatarTypes =
+export type AvatarTypes =
   | "avatar1"
   | "avatar2"
   | "avatar3"
@@ -10,9 +10,9 @@ type AvatarTypes =
   | undefined;
 
 /**
- * Returns the Avatar based on the profile's chosen avatar.
+ * Returns the Avatar based on the avatar prop
  */
-export function ProfileAvatar(avatar: AvatarTypes) {
+export function ProfileAvatar({ avatar }: { avatar: AvatarTypes }) {
   // TODO: Change this to the actual mapping to the correct avatar images
   // TODO (BUG): On page refresh, or any situation that turns profile = null, it defaults to the image, creating a janky look when profile is initialize
   const avatarMaps = {
