3337583c5c83e41a1dd39a813da35e728efaa735
Hao Phan <rinnphan01@gmail.com>
2024-10-15 00:48:21 -0700

    Temporary stage

--- client/src/app/(main)/track/page.tsx
+++ client/src/app/(main)/track/page.tsx
@@ -2,36 +2,53 @@ import { Typography } from "@mui/material";
 import "./tracking.css";
 
 export default function Page() {
-  const num_applications = 5; // dummy data for number of applications
+  const num_applications = "<Dummy Holder>"; // dummy data for number of applications
   const stages = [
-    { name: "Initial Screen", color: "blue" },
-    { name: "Rejected", color: "red" },
-    { name: "Online Assessment", color: "orange" },
-    { name: "Interviewing", color: "yellow" },
-    { name: "Offer", color: "green" },
+    { name: "Initial Screen", color: "#769FCD" },
+    { name: "Rejected", color: "#C7253E" },
+    { name: "Online Assessment", color: "#EB5B00" },
+    { name: "Interviewing", color: "#F0A202" },
+    { name: "Offer", color: "#2E7E33" },
   ];
 
   return (
     <div className="main">
       <Typography variant="h6">Submitted Applications</Typography>
-      <Typography variant="subtitle1">Total Applications: {num_applications}</Typography>
+      <Typography variant="subtitle1">
+        Total Applications: {num_applications}
+      </Typography>
 
+      {/* this is for the new application button and search box */}
+      <div></div>
+
+      {/* this is for the application stages */}
       <div className="application-stages">
         {stages.map((stage, index) => (
           <div key={index} className="stage-column">
-            <Typography variant="subtitle2" className="stage-title">
+            <Typography
+              variant="subtitle2"
+              sx={{
+                margin: "15px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
               <span
-                className="stage-circle"
-                style={{ backgroundColor: stage.color }}
+                style={{
+                  backgroundColor: stage.color, 
+                  display: "inline-block",
+                  width: "12px",
+                  height: "12px",
+                  borderRadius: "50%",
+                  marginRight: "8px",
+                }}
               ></span>
-              {stage.name} (0)
+              {stage.name} (num_applications)
             </Typography>
             <div className="application-box">+</div>
           </div>
         ))}
       </div>
-
-      
     </div>
   );
 }
