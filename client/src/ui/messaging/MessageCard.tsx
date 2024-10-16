a0f3cf27e888fecc46eec90af780fd844a68e0e3
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-16 09:42:44 -0700

    implemented date divider separator for messages

--- client/src/ui/messaging/MessageCard.tsx
+++ client/src/ui/messaging/MessageCard.tsx
@@ -3,6 +3,7 @@ import { AvatarTypes, ProfileAvatar } from "../ProfileAvatar";
 import { memo } from "react";
 import { MutableRefObject } from "react";
 import { useProfile } from "../../../lib/store/profile";
+import { DividerWithText } from "../DividerWithText";
 export interface MessageCardProps {
   avatar: AvatarTypes;
   sender_username: string;
@@ -31,6 +32,8 @@ export const MessageCard = memo(function MessageCard({
   const { profile } = useProfile();
   const messageDate = new Date(timestamp);
 
+  // TODO: Decouple boolean logic and setting prevDate state
+  // TODO: This needs to get tested
   function doRenderDivider(): boolean {
     // if prevDate is passed in
     if (prevDate) {
@@ -40,47 +43,59 @@ export const MessageCard = memo(function MessageCard({
         (prevDate.current && prevDate.current.getDay() !== messageDate.getDay())
       ) {
         console.log("render divider");
+        prevDate.current = messageDate;
 
-        if (!prevDate.current) {
-          prevDate.current = messageDate;
-        }
         return true;
       }
     }
     return false;
   }
 
-  doRenderDivider();
-
+  const res = doRenderDivider();
   return (
-    <Card
-      sx={{
-        boxShadow: "none",
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <CardHeader
-        avatar={<ProfileAvatar avatar={avatar} />}
-        title={sender_username}
-        subheader={
-          messageDate.toLocaleDateString("en-US", {
-            month: "2-digit",
-            day: "2-digit",
-            year: "2-digit",
-          }) +
-          " " +
-          messageDate.toLocaleTimeString("en-US")
-        }
-        titleTypographyProps={{
-          color: profile?.username === sender_username ? "#496FFF" : "#3C435C",
-        }}
-      />
-      <CardContent
+      {/* TODO: Clean this up to make it simpler */}
+      {res && prevDate?.current && (
+        <Typography>{prevDate.current.toDateString()}</Typography>
+      )}
+      <Card
         sx={{
-          marginTop: "-20px",
+          boxShadow: "none",
+          width: "100%",
         }}
       >
-        <Typography>{message}</Typography>
-      </CardContent>
-    </Card>
+        <CardHeader
+          avatar={<ProfileAvatar avatar={avatar} />}
+          title={sender_username}
+          subheader={
+            messageDate.toLocaleDateString("en-US", {
+              month: "2-digit",
+              day: "2-digit",
+              year: "2-digit",
+            }) +
+            " " +
+            messageDate.toLocaleTimeString("en-US")
+          }
+          titleTypographyProps={{
+            color:
+              profile?.username === sender_username ? "#496FFF" : "#3C435C",
+          }}
+        />
+        <CardContent
+          sx={{
+            marginTop: "-20px",
+          }}
+        >
+          <Typography>{message}</Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 });
