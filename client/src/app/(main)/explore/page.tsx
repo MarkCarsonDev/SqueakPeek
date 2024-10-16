bc505bf081fad77abbeb01af9c2b3ef909f1e4a0
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-10-16 12:32:58 -0700

    added chips

--- client/src/app/(main)/explore/page.tsx
+++ client/src/app/(main)/explore/page.tsx
@@ -3,9 +3,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from "next/navigation";
 import { Filters, SelectedFilters, FilterOption } from "@/ui/explore/Filters";
-import { Typography, Avatar } from '@mui/material';
+import { Typography, Avatar } from "@mui/material";
 import { OpportunityCard } from "@/ui/explore/OpportunityCard";
 import "@/app/(main)/explore/explore.css";
 import FilteredContentComponent from "@/ui/explore/FilteredContentComponent";
@@ -16,9 +16,9 @@ export default function Page() {
 
   // Define getInitialFilters function
   const getInitialFilters = (): SelectedFilters => {
-    const jobTypes = searchParams.getAll('jobType');
-    const appliedStatuses = searchParams.getAll('appliedStatus');
-    const years = searchParams.getAll('year');
+    const jobTypes = searchParams.getAll("jobType");
+    const appliedStatuses = searchParams.getAll("appliedStatus");
+    const years = searchParams.getAll("year");
 
     return {
       jobTypes,
@@ -36,51 +36,84 @@ export default function Page() {
   useEffect(() => {
     const queryParams = new URLSearchParams();
 
-    filters.jobTypes.forEach((value) => queryParams.append('jobType', value));
+    filters.jobTypes.forEach((value) => queryParams.append("jobType", value));
     filters.appliedStatuses.forEach((value) =>
-      queryParams.append('appliedStatus', value)
+      queryParams.append("appliedStatus", value)
     );
-    filters.years.forEach((value) => queryParams.append('year', value));
+    filters.years.forEach((value) => queryParams.append("year", value));
 
     router.replace(`?${queryParams.toString()}`);
   }, [filters, router]);
 
   // Mock data for filter options
   const jobTypes: FilterOption[] = [
-    { label: 'Full-Time', value: 'full-time' },
-    { label: 'Part-Time', value: 'part-time' },
-    { label: 'Internship', value: 'internship' },
+    { label: "Full-Time", value: "full-time" },
+    { label: "Part-Time", value: "part-time" },
+    { label: "Internship", value: "internship" },
   ];
 
   const appliedStatuses: FilterOption[] = [
-    { label: 'Applied', value: 'applied' },
-    { label: 'Not Applied', value: 'not-applied' },
+    { label: "Applied", value: "applied" },
+    { label: "Not Applied", value: "not-applied" },
   ];
 
   const years: FilterOption[] = [
-    { label: '2021', value: '2021' },
-    { label: '2022', value: '2022' },
-    { label: '2023', value: '2023' },
+    { label: "2021", value: "2021" },
+    { label: "2022", value: "2022" },
+    { label: "2023", value: "2023" },
   ];
 
   return (
     <div className="page-container">
       <div className="header-search-container">
         <div className="header-text">
-          <Typography sx={{margin: "1rem 1rem"}} variant="h5"><span style={{ fontWeight: "bold" }}>Explore</span> 321 Results</Typography>
-          <Typography sx={{marginLeft: "1rem"}} variant="body1">Explore entry-level roles, discover the application pipeline,</Typography>
-          <Typography sx={{marginLeft: "1rem"}} variant="body1">and talk to other applicants in the <span style={{ fontWeight: "bold"}}>company threads</span>.</Typography>
+          <Typography sx={{ margin: "1rem 1rem" }} variant="h5">
+            <span style={{ fontWeight: "bold" }}>Explore</span> 321 Results
+          </Typography>
+          <Typography sx={{ marginLeft: "1rem" }} variant="body1">
+            Explore entry-level roles, discover the application pipeline,
+          </Typography>
+          <Typography sx={{ marginLeft: "1rem" }} variant="body1">
+            and talk to other applicants in the{" "}
+            <span style={{ fontWeight: "bold" }}>company threads</span>.
+          </Typography>
         </div>
-
       </div>
       <div className="card-filter-container">
         <div className="opportunity-column">
           {/* Render FilteredContentComponent here */}
-          <OpportunityCard title="Amazon" jobAvatar={<Image src="/landingpage/insight.svg" height={50} width={50} alt='Logo'/>} dateRangeStart="2023" dateRangeEnd="2024" jobPosition="Software Developer" jobType="Full-Time" positionStatus={false} userPositionStatus={false} />
+          <OpportunityCard
+            title="Amazon"
+            jobAvatar={
+              <Image
+                src="/landingpage/insight.svg"
+                height={50}
+                width={50}
+                alt="Logo"
+              />
+            }
+            dateRangeStart="2023"
+            dateRangeEnd="2024"
+            jobPosition="Software Developer"
+            jobType="Full-Time"
+            positionStatus={false}
+            userPositionStatus={false}
+            totalApplied={100}
+            rejected={50}
+            oa={50}
+            interviewing={50}
+            offered={50}
+
+          />
         </div>
 
         <div className="filter-column">
-          <Typography variant="h6" style={{ marginBottom: "5px", marginLeft: ".5rem" }}>Filters</Typography>
+          <Typography
+            variant="h6"
+            style={{ marginBottom: "5px", marginLeft: ".5rem" }}
+          >
+            Filters
+          </Typography>
           {/* Render Filters component here */}
           <Filters
             jobTypes={jobTypes}
--- client/src/ui/explore/OpportunityCard.tsx
+++ client/src/ui/explore/OpportunityCard.tsx
@@ -6,10 +6,16 @@ import {
   CardHeader,
   CardContent,
   Typography,
-} from '@mui/material';
-import { faAnglesUp, IconDefinition, faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+  IconButton,
+  Chip,
+} from "@mui/material";
+import {
+  faAnglesUp,
+  IconDefinition,
+  faAnglesDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from 'next/image';
+import Image from "next/image";
 
 interface OpportunityCardProps {
   title: string;
@@ -20,8 +26,18 @@ interface OpportunityCardProps {
   jobType: string;
   positionStatus: boolean;
   userPositionStatus: boolean;
+  totalApplied: number;
+  rejected: number;
+  oa: number;
+  interviewing: number;
+  offered: number;
 }
 
+interface jobStats {
+  status: string;
+  borderColor: string;
+  quantity: number;
+}
 
 export function OpportunityCard({
   title,
@@ -31,13 +47,47 @@ export function OpportunityCard({
   jobPosition,
   jobType,
   positionStatus,
-  userPositionStatus
+  userPositionStatus,
+  totalApplied,
+  rejected,
+  oa,
+  interviewing,
+  offered,
 }: OpportunityCardProps) {
+  const stats: jobStats[] = [
+    {
+      status: "Total Applied:",
+      borderColor: "1px solid black",
+      quantity: totalApplied,
+    },
+    {
+      status: "Rejected:",
+      borderColor: "1px solid red",
+      quantity: rejected,
+    },
+    {
+      status: "OA:",
+      borderColor: "1px solid orange",
+      quantity: oa,
+    },
+    {
+      status: "Interviewing:",
+      borderColor: "1px solid yellow",
+      quantity: interviewing,
+    },
+    {
+      status: "Offered:",
+      borderColor: "1px solid green",
+      quantity: offered,
+    },
+  ];
 
   return (
-    <Card sx={{ border: 'solid 3px #e0e4f2', margin: '3rem', borderRadius: "20px"}}>
+    <Card
+      sx={{ border: "solid 3px #e0e4f2", margin: "3rem", borderRadius: "20px" }}
+    >
       <CardHeader
-        sx={{ height: '25px' }}
+        sx={{ height: "25px" }}
         avatar={jobAvatar}
         title={<Typography>{title}</Typography>}
         subheader={
@@ -49,44 +99,62 @@ export function OpportunityCard({
       <Typography variant="h5" sx={{ marginInline: "1.5rem" }}>
         {jobPosition}, {jobType}
       </Typography>
-      <CardContent sx={{display: "flex", gap: "1rem"}}>
+      <CardContent sx={{ display: "flex", gap: "1rem" }}>
         <Typography
           sx={{
             display: "flex",
             alignItems: "center",
             padding: ".25rem .50rem",
-            border: userPositionStatus ? "solid 2px green": "solid 2px red",
+            border: userPositionStatus ? "solid 2px green" : "solid 2px red",
             borderRadius: "10px",
             minWidth: "auto",
             height: "15px",
             textAlign: "center",
           }}
-          variant='body2'
+          variant="body2"
         >
-          {userPositionStatus? "Applied" : "Not Applied"}
+          {userPositionStatus ? "Applied" : "Not Applied"}
         </Typography>
         <Typography
           sx={{
             display: "flex",
             alignItems: "center",
             padding: ".25rem .50rem",
-            border: positionStatus ? "solid 2px green": "solid 2px red",
+            border: positionStatus ? "solid 2px green" : "solid 2px red",
             borderRadius: "10px",
             height: "15px",
             minWidth: "auto",
             textAlign: "center",
           }}
         >
-          {userPositionStatus? "Actively Hiring" : "Not Hiring"} <FontAwesomeIcon style={{marginLeft: ".25rem"}} icon={positionStatus ? faAnglesUp :faAnglesDown }/>
+          {userPositionStatus ? "Actively Hiring" : "Not Hiring"}{" "}
+          <FontAwesomeIcon
+            style={{ marginLeft: ".25rem" }}
+            icon={positionStatus ? faAnglesUp : faAnglesDown}
+          />
         </Typography>
       </CardContent>
-      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+      <CardContent
+        style={{
+          display: "flex",
+          justifyContent: "end",
+          gap: "1rem",
+          marginRight: "3rem",
+        }}
+      >
+        {stats.map((stats) => (
+            <Chip key={stats.status} label={`${stats.status} ${stats.quantity}`} variant="outlined" sx={{color: "red", borderColor: "red"}}/>
+        ))}
+      </CardContent>
+      <CardContent sx={{ display: "flex", justifyContent: "center" }}>
+        <IconButton></IconButton>
         <Image
-        src="/explore/opportunityLine.svg"
-        alt="Opportunity Line"
-        height={100}
-        width={1200}
+          src="/explore/opportunityLine.svg"
+          alt="Opportunity Line"
+          height={100}
+          width={1100}
         />
+        <IconButton></IconButton>
       </CardContent>
     </Card>
   );
