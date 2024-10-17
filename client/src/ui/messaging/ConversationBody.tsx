2026062d9dafb4437d49f80c441fae0c22175931
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-16 20:54:44 -0700

    added message chip UI, implementation missing

--- client/src/ui/messaging/ConversationBody.tsx
+++ client/src/ui/messaging/ConversationBody.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useMessage } from "../../../lib/store/message";
 import { createSupabaseClient } from "../../../lib/supabase/client";
 import { useEffect, useRef } from "react";
+import { Chip } from "@mui/material";
 
 interface ConversationBodyProps {
   conversationId: string;
@@ -70,6 +71,24 @@ export function ConversationBody({ conversationId }: ConversationBodyProps) {
           overflowY: "auto", // allows scrolling on the messages
         }}
       >
+        <Chip
+          style={{}}
+          label="2 new messages"
+          variant="outlined"
+          sx={{
+            position: "absolute",
+            margin: "0 auto",
+            top: "20%",
+            borderColor: "#E0E4F2",
+            borderWidth: "2px",
+            boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+
+            // centers chip horizontally relative to the
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+          onClick={() => {}}
+        />
         {messages.map((message) => {
           return (
             <MessageCard
