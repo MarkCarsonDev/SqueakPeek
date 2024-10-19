5b696eef1c10c4afe65cddcb52159df2e83fc026
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-19 12:57:49 -0700

    fix build issue

--- client/src/app/(main)/track/page.tsx
+++ client/src/app/(main)/track/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Button, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileCirclePlus } from "@fortawesome/free-solid-svg-icons";
-import NewApplicationModal from "@/ui/track/NewApplicationModal";
+import NewApplicationModal from "@/ui/track/newApplicationModal";
 import "./tracking.css";
 
 export default function Page() {
@@ -17,35 +17,34 @@ export default function Page() {
   return (
     <div className="main">
       <Typography variant="h3">Submitted Applications</Typography>
-      <Typography variant="subtitle1">Total Applications: {num_applications}</Typography>
+      <Typography variant="subtitle1">
+        Total Applications: {num_applications}
+      </Typography>
 
-        <Button
-          variant="contained"
-          endIcon={<FontAwesomeIcon icon={faFileCirclePlus} />}
-          sx={{
-            mt: 2,
-            width: "200px",
+      <Button
+        variant="contained"
+        endIcon={<FontAwesomeIcon icon={faFileCirclePlus} />}
+        sx={{
+          mt: 2,
+          width: "200px",
+          boxShadow: "none",
+          backgroundColor: "#496FFF",
+          borderRadius: "8px",
+          ":hover": {
+            backgroundColor: "#3B5AC6",
             boxShadow: "none",
-            backgroundColor: "#496FFF",
-            borderRadius: "8px",
-            ":hover": {
-              backgroundColor: "#3B5AC6",
-              boxShadow: "none",
-            },
-          }}
-          onClick={handleOpenModal}
-        >
-          New Application
-        </Button>
+          },
+        }}
+        onClick={handleOpenModal}
+      >
+        New Application
+      </Button>
 
       {/* The New Application Modal */}
-      <NewApplicationModal open={openModal} handleClose={handleCloseModal}/>
-          
+      <NewApplicationModal open={openModal} handleClose={handleCloseModal} />
     </div>
   );
 }
-
-
 
 // Will comeback to this later
 // const stages = [
@@ -55,7 +54,8 @@ export default function Page() {
 //   { name: "Interviewing", color: "#F0A202" },
 //   { name: "Offer", color: "#2E7E33" },
 // ];
-{/* <div className="application-stages">
+{
+  /* <div className="application-stages">
         {stages.map((stage, index) => (
           <div key={index} className="stage-column">
             <Typography
@@ -81,4 +81,5 @@ export default function Page() {
             <div className="application-box">+</div>
           </div>
         ))}
-      </div> */}
+      </div> */
+}
