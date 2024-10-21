7cf2f52851778b8072796334f00653570f132a36
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-20 20:11:11 -0700

    applied layout in explore[id] page

--- client/src/app/(main)/explore/[id]/page.tsx
+++ client/src/app/(main)/explore/[id]/page.tsx
@@ -2,8 +2,14 @@ import { Conversation } from "@/ui/messaging/Conversation";
 export default function Page({ params }: { params: { id: string } }) {
   const { id: conversationId } = params;
   return (
-    <div>
+    <div style={{
+      display:"grid",
+      gridAutoColumns:"60% 40%"
+    }}>
       <Conversation conversationId={conversationId} />
+      <div>
+        
+      </div>
     </div>
   );
 }
