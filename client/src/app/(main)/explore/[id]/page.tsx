6bdd8ac951bf5e65286aa7c7073c146b50ca8307
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-20 20:58:44 -0700

    fixed layout bug

--- client/src/app/(main)/explore/[id]/page.tsx
+++ client/src/app/(main)/explore/[id]/page.tsx
@@ -1,14 +1,17 @@
 import { Conversation } from "@/ui/messaging/Conversation";
+import { Typography } from "@mui/material";
 export default function Page({ params }: { params: { id: string } }) {
   const { id: conversationId } = params;
   return (
-    <div style={{
-      display:"grid",
-      gridAutoColumns:"60% 40%"
-    }}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "60% 40%",
+      }}
+    >
       <Conversation conversationId={conversationId} />
       <div>
-        
+        <Typography>Hello World</Typography>
       </div>
     </div>
   );
