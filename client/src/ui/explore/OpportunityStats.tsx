857f86ad064654a333cd598466f3b2cc99f81022
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-21 11:12:17 -0700

    added MUI pie chart

--- client/src/ui/explore/OpportunityStats.tsx
+++ client/src/ui/explore/OpportunityStats.tsx
@@ -1,4 +1,11 @@
+"use client";
 import { Typography } from "@mui/material";
+import {
+  PieChart,
+  pieArcLabelClasses,
+  BarChart,
+  LineChart,
+} from "@mui/x-charts";
 
 export function OpportunityStats() {
   return (
@@ -8,7 +15,37 @@ export function OpportunityStats() {
       }}
     >
       <Typography variant="h3">Stats</Typography>
-      
+      <PieChart
+        series={[
+          {
+            arcLabel: (item) => `${item.value}`,
+            arcLabelMinAngle: 35,
+            data: [
+              { id: 0, value: 90, label: "Applied", color: "#779fcd" },
+              { id: 1, value: 190, label: "Rejected", color: "#c7253e" },
+              {
+                id: 2,
+                value: 30,
+                label: "OA",
+                color: "#eb5a02",
+              },
+              { id: 3, value: 20, label: "Interviewing", color: "#ffbf63" },
+
+              { id: 4, value: 10, label: "Offered", color: "#2e7e33" },
+            ],
+          },
+        ]}
+        sx={{
+          fontFamily: "",
+          [`& .${pieArcLabelClasses.root}`]: {
+            fontWeight: "bold",
+            fill: "white",
+            color: "white",
+          },
+        }}
+        width={450}
+        height={250}
+      />
     </div>
   );
 }
