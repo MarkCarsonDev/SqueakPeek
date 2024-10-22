1fc05c0104e9cea4849d3c98f8589dca6b26e361
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-22 14:27:31 -0700

    fixed build errors

--- client/src/app/(main)/explore/[id]/page.tsx
+++ client/src/app/(main)/explore/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { OpportunityStats } from "@/ui/explore/OpportunityStats";
 import { Conversation } from "@/ui/messaging/Conversation";
-import { Typography } from "@mui/material";
 export default function Page({ params }: { params: { id: string } }) {
   const { id: conversationId } = params;
   return (
