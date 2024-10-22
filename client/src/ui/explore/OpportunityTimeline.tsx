5fa7fc82575a1ca67e1b3ccc2210a26922ebdc2d
Serranoda <Serranoda21@gmail.com>
2024-10-22 08:20:20 -0700

    mapped data for timeline, and fixed reference error

--- client/src/ui/explore/OpportunityTimeline.tsx
+++ client/src/ui/explore/OpportunityTimeline.tsx
@@ -2,11 +2,32 @@
 import { Chrono } from "react-chrono";
 import "@/ui/explore/OpportunityTimeline.css";
 import { Typography } from "@mui/material";
+import { useEffect, useState } from 'react';
 
 interface oppornityDataProps {
   id: number;
 }
+
+interface rangeStats {
+  interviewing: number
+  rejected: number
+}
+
+interface timelinePoints {
+  dateRangeStart: string
+  dateRangeEnd: string
+}
 export function OpportunityTimeline({ id }: oppornityDataProps) {
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Render nothing on the server side
+  }
   //future function for fetching timeline data
   const fetchTimelineData = (oppId: number) => {
     return console.log(oppId);
@@ -14,57 +35,67 @@ export function OpportunityTimeline({ id }: oppornityDataProps) {
 
   fetchTimelineData(id);
 
+  const rstats: rangeStats [] = [
+    {
+      interviewing: 25,
+      rejected: 25
+    },
+    {
+      interviewing: 25,
+      rejected: 25
+    },
+    {
+      interviewing: 25,
+      rejected: 25
+    },
+    {
+      interviewing: 25,
+      rejected: 25
+    },
+    {
+      interviewing: 25,
+      rejected: 25
+    },
+  ]
+
+  const tPoints: timelinePoints [] = [
+    {
+      dateRangeStart: "4/29/24",
+      dateRangeEnd: "5/7/24"
+    },
+    {
+      dateRangeStart: "4/29/24",
+      dateRangeEnd: "5/7/24"
+    },
+    {
+      dateRangeStart: "4/29/24",
+      dateRangeEnd: "5/7/24"
+    },
+    {
+      dateRangeStart: "4/29/24",
+      dateRangeEnd: "5/7/24"
+    },
+    {
+      dateRangeStart: "4/29/24",
+      dateRangeEnd: "5/7/24"
+    },
+  ]
   // Hard coded values for content card
-  const customContent = [
-    <div key={1} style={{ textAlign: "center" }}>
+  const customContent = 
+    rstats.map((rstats, index) => (
+      <div key={index} style={{ textAlign: "center" }}>
       <Typography>Job Stats</Typography>
-      <Typography>Interviewing: 50</Typography>
-      <Typography>Rejected: 50</Typography>
-    </div>,
-    <div key={2} style={{ textAlign: "center" }}>
-      <Typography>Job Stats</Typography>
-      <Typography>Interviewing: 25</Typography>
-      <Typography>Rejected: 25</Typography>
-    </div>,
-    <div key={3} style={{ textAlign: "center" }}>
-      <Typography>Job Stats</Typography>
-      <Typography>Interviewing: 25</Typography>
-      <Typography>Rejected: 25</Typography>
-    </div>,
-    <div key={4} style={{ textAlign: "center" }}>
-      <Typography>Job Stats</Typography>
-      <Typography>Interviewing: 25</Typography>
-      <Typography>Rejected: 25</Typography>
-    </div>,
-    <div key={5} style={{ textAlign: "center" }}>
-      <Typography>Job Stats</Typography>
-      <Typography>Interviewing: 25</Typography>
-      <Typography>Rejected: 25</Typography>
-    </div>,
-  ];
+      <Typography>Interviewing: {rstats.interviewing}</Typography>
+      <Typography>Rejected: {rstats.rejected}</Typography>
+    </div>
+    ))
+    
 
   //Hard coded Point titles
-  const items = [
-    {
-      title: <Typography color="white">04/29/24 - 05/7/24</Typography>,
-    },
-    {
-      title: <Typography color="white">05/8/24 - 05/15/24</Typography>,
-    },
-    {
-      title: <Typography color="white">05/16/24 - 05/23/24</Typography>,
-    },
-    {
-      title: <Typography color="white">05/24/24 - 05/30/24</Typography>,
-    },
-    {
-      title: <Typography color="white">05/30/24 - 06/06/24</Typography>,
-    },
-    {
-      title: <Typography color="white">05/30/24 - 06/06/24</Typography>,
-    },
-  ];
-
+  const items = tPoints.map((tPoints, index) => ({
+    title: <Typography key={index} color="white">{tPoints.dateRangeStart} - {tPoints.dateRangeEnd}</Typography>,
+  }));
+    
   return (
     <div className="custom-timeline">
       <Chrono
