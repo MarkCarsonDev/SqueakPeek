36a1a25d319f83717f98e8241c5f326d82bcc04f
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-23 01:01:05 -0700

    added section for private in message page

--- client/src/app/(main)/message/private/page.tsx
+++ client/src/app/(main)/message/private/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Typography } from "@mui/material";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 export default function page() {
-  return <div>Private</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundColor: "white",
+        height: "100%",
+      }}
+    >
+      <FontAwesomeIcon
+        style={{
+          border: "3px solid #E0E4F2",
+          padding: "25px",
+          borderRadius: "100px",
+          marginBottom: "20px",
+        }}
+        size="3x"
+        icon={faPaperPlane}
+      />
+      <Typography variant="h4">Direct Messages</Typography>
+      <Typography variant="h6">
+        Chat privately with other applicants in the pipeline
+      </Typography>
+    </div>
+  );
 }
