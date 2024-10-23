db473427235bd900c2efafc18c61579104be44bf
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-22 20:07:41 -0700

    removed uncessary code

--- client/src/app/(main)/message/page.tsx
+++ client/src/app/(main)/message/page.tsx
@@ -1,29 +1,5 @@
 "use client";
 import { Typography } from "@mui/material";
-import { useEffect } from "react";
-import { createSupabaseClient } from "../../../lib/supabase/client";
 export default function Page() {
-  useEffect(() => {
-    const supabase = createSupabaseClient();
-    const channelB = supabase.channel("room-1");
-    // Join a room/topic. Can be anything except for 'realtime'.
-
-    channelB.subscribe((status) => {
-      // Wait for successful connection
-      console.log("status: ", status);
-      if (status !== "SUBSCRIBED") {
-        console.log("failure");
-        return null;
-      }
-
-      // Send a message once the client is subscribed
-      channelB.send({
-        type: "broadcast",
-        event: "test",
-        payload: { message: "hello, world" },
-      });
-    });
-  }, []);
-
   return <Typography variant="h2">Message</Typography>;
 }
