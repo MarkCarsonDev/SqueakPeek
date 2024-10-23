f09a285748f347bfe50f4065824e5b8844594d1a
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-22 23:05:42 -0700

    added solid and regular font awesome button icons based on current pathname

--- client/src/ui/messaging/SideNav.tsx
+++ client/src/ui/messaging/SideNav.tsx
@@ -1,9 +1,24 @@
+"use client";
 import { Typography, Tabs, Tab } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding as solidBuilding,
+  faMessage as solidMessage,
+} from "@fortawesome/free-solid-svg-icons";
 
+import {
+  faBuilding as regularBuilding,
+  faMessage as regularMessage,
+} from "@fortawesome/free-regular-svg-icons";
+
+import { usePathname } from "next/navigation";
 /**
  * Allows the user to navigate between company threads or private messages in the message page
  */
 export function SideNav() {
+  const pathName = usePathname();
+  console.log("pathName: ", pathName);
+
   return (
     <div>
       {" "}
@@ -19,8 +34,34 @@ export function SideNav() {
         Talk to other applicants in the process, or talk privately
       </Typography>
       <Tabs>
-        <Tab href="company" label="Company Threads"></Tab>
-        <Tab href="private" label="Private Messages"></Tab>
+        <Tab
+          icon={
+            <FontAwesomeIcon
+              size="2x"
+              icon={
+                pathName === "/message/company"
+                  ? solidBuilding
+                  : regularBuilding
+              }
+            />
+          }
+          href="company"
+          label="Company Threads"
+          iconPosition="start"
+        />
+        <Tab
+          icon={
+            <FontAwesomeIcon
+              size="2x"
+              icon={
+                pathName === "/message/private" ? solidMessage : regularMessage
+              }
+            />
+          }
+          href="private"
+          label="Private Messages"
+          iconPosition="start"
+        />
       </Tabs>
     </div>
   );
