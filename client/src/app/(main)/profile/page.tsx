0d055824067014cea3293e126b1db14ab06aa2c9
Serranoda <112042205+Serranoda@users.noreply.github.com>
2024-10-23 10:55:15 -0700

    removed import

--- client/src/app/(main)/profile/page.tsx
+++ client/src/app/(main)/profile/page.tsx
@@ -1,5 +1,4 @@
 import { Typography } from "@mui/material";
-import { OpportunityStackedBarGraph } from "@/ui/explore/OpportunityStackedBarGraph";
 export default function Page() {
   return <Typography variant="h2">Profile</Typography>;
 }
