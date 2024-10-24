c89f62650a4794dfd3193d91304939ae6304c194
Hao Phan <rinnphan01@gmail.com>
2024-10-23 19:40:00 -0700

    remove unused

--- client/src/lib/store/Tracking/ApplicationAction.ts
+++ client/src/lib/store/Tracking/ApplicationAction.ts
@@ -11,21 +11,21 @@ export interface Application {
     jobURL: string;
   }
   
-  // Function to add an application to the store
-  export const addApplicationToStore = (
-    stageStore: { addApplication: (app: Application) => void },
-    application: Application
-  ) => {
-    stageStore.addApplication(application);
-  };
+  // // Function to add an application to the store
+  // export const addApplicationToStore = (
+  //   stageStore: { addApplication: (app: Application) => void },
+  //   application: Application
+  // ) => {
+  //   stageStore.addApplication(application);
+  // };
   
-  // Function to move an application between stores (stages)
-  export const moveApplicationBetweenStores = (
-    sourceStore: { removeApplication: (appId: string) => void },
-    destinationStore: { addApplication: (app: Application) => void },
-    applicationId: string,
-    application: Application
-  ) => {
-    sourceStore.removeApplication(applicationId);
-    destinationStore.addApplication(application);
-  };
+  // // Function to move an application between stores (stages)
+  // export const moveApplicationBetweenStores = (
+  //   sourceStore: { removeApplication: (appId: string) => void },
+  //   destinationStore: { addApplication: (app: Application) => void },
+  //   applicationId: string,
+  //   application: Application
+  // ) => {
+  //   sourceStore.removeApplication(applicationId);
+  //   destinationStore.addApplication(application);
+  // };
