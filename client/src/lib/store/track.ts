214112630073777bc71b53401e30be5c849ae0d1
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-10-24 00:47:33 -0700

    set up track store, methods have no implementation yet

---
+++ client/src/lib/store/track.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+
+type ApplicationStage =
+  | "applied"
+  | "rejected"
+  | "onlineAssessment"
+  | "interviewing"
+  | "offer";
+
+interface Application {
+  id: string;
+  roleTitle: string;
+  companyName: string;
+  location: string;
+  jobtype: string;
+  dateApplied: string;
+  applicationURL: string;
+  applicationStatus: ApplicationStage;
+}
+
+interface TrackState {
+  applied: Application[];
+  rejected: Application[];
+  onlineAssessment: Application[];
+  interviewing: Application[];
+  offer: Application[];
+  addApplication: (to: ApplicationStage, application: Application) => void;
+  removeApplication: (from: ApplicationStage, applicationId: string) => void;
+  moveApplication: (
+    from: ApplicationStage,
+    to: ApplicationStage,
+    applicationId: string
+  ) => void;
+}
+
+export const useTrack = create<TrackState>()((set) => ({
+  applied: [],
+  rejected: [],
+  onlineAssessment: [],
+  interviewing: [],
+  offer: [],
+  addApplication: () => {},
+  removeApplication: () => {},
+  moveApplication: () => {},
+}));
