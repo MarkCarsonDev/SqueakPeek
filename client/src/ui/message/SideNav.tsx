cb0760b57c8ef57f22022e164f1fb2c29b02b96b
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-11-01 19:57:44 -0700

    added TODO comment in SideNav

--- client/src/ui/message/SideNav.tsx
+++ client/src/ui/message/SideNav.tsx
@@ -72,6 +72,7 @@ export function SideNav() {
           }
         });
       } else {
+        //TODO: Fetch bookmarked opportunities here
         setMessageNotificationsList([
           {
             avatar: "test",
