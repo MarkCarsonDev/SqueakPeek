b6802f04ffaa272dc431053fe81470c47a1a603a
Noah Daniels <noahjdaniels@gmail.com>
2024-11-19 10:51:22 -0800

    origin main pull

---
+++ client/src/lib/store/initProfile 3.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useProfile } from "./profile";
+import { useEffect } from "react";
+import { User } from "@supabase/supabase-js";
+import { createSupabaseClient } from "../supabase/client";
+
+/**
+ * Used as a profile store to be accessible throughout the pages
+ * NOTE: This is not a UI component which is why it returns null
+ */
+export default function InitProfile({ user }: { user: User | null }) {
+  const { profile, initializeProfile } = useProfile((state) => state);
+  useEffect(() => {
+    // only calls when an authenticated user doesn't have their profile initialized
+    if (user && profile === null) {
+      const supabase = createSupabaseClient();
+      supabase
+        .from("profile")
+        .select("*")
+        .eq("user_id", user.id)
+        .single()
+        .then((res) => {
+          const { data, error } = res;
+
+          if (!error && data) {
+            initializeProfile(data);
+          }
+        });
+    }
+  }, [profile, initializeProfile, user]);
+  return null;
+}
---
+++ client/src/ui/message/SideNavTabsList 2.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  faBuilding as solidBuilding,
+  faMessage as solidMessage,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faBuilding as regularBuilding,
+  faMessage as regularMessage,
+} from "@fortawesome/free-regular-svg-icons";
+import { useRouter } from "next/navigation";
+import { Typography, Tabs, Tab } from "@mui/material";
+
+const tabs = [
+  {
+    label: "Company Threads",
+    tabPathName: "company",
+    solidIcon: solidBuilding,
+    regularIcon: regularBuilding,
+  },
+  {
+    label: "Private",
+    tabPathName: "private",
+    solidIcon: solidMessage,
+    regularIcon: regularMessage,
+  },
+];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useMessageNotification } from "@/lib/store/messageNotification";
+interface SideNavTabsListProps {
+  currentTab: string;
+}
+export default function SideNavTabsList({ currentTab }: SideNavTabsListProps) {
+  const router = useRouter();
+  const { privateNotifications } = useMessageNotification();
+  const numPrivateNotifications = privateNotifications.filter(
+    (notification) => notification.isRead === false
+  ).length;
+  return (
+    <Tabs
+      value={currentTab}
+      TabIndicatorProps={{
+        style: {
+          background: "#496FFF",
+        },
+      }}
+      sx={{
+        marginTop: "20px",
+        justifySelf: "center",
+        display: "flex",
+        justifyContent: "center",
+        justifyItems: "center",
+        "&.Mui-selected": {
+          color: "yellow",
+          opacity: 1,
+        },
+        "&.Mui-focusVisible": {
+          backgroundColor: "#d1eaff",
+        },
+      }}
+    >
+      {tabs.map(({ tabPathName, solidIcon, regularIcon, label }) => (
+        <Tab
+          value={tabPathName}
+          key={tabPathName}
+          sx={{
+            padding: "0px",
+            marginRight: "20px",
+            "&.Mui-selected": {
+              color: "#496FFF",
+            },
+          }}
+          icon={
+            <>
+              {tabPathName === "private" && numPrivateNotifications > 0 && (
+                <Typography
+                  sx={{
+                    color: "white",
+                    position: "absolute",
+                    width: "20px",
+                    height: "20px",
+                    bottom: "65%",
+                    right: "20%",
+                    backgroundColor: "#496FFF",
+                    borderRadius: "20px",
+                    border: "2px solid #F6F8FF",
+                    marginTop: "2px",
+                  }}
+                  variant="caption"
+                >
+                  {numPrivateNotifications <= 9
+                    ? numPrivateNotifications
+                    : "9+"}
+                </Typography>
+              )}
+              <FontAwesomeIcon
+                size="2x"
+                icon={
+                  currentTab === tabPathName // gets the route name after the /message route
+                    ? solidIcon
+                    : regularIcon
+                }
+              />
+            </>
+          }
+          onClick={() => router.push(`/message/${tabPathName}`)}
+          label={
+            <Typography
+              sx={{
+                color: currentTab === tabPathName ? "#496FFF" : "#3C435C",
+              }}
+              variant="caption"
+            >
+              {label}
+            </Typography>
+          }
+        />
+      ))}
+    </Tabs>
+  );
+}
