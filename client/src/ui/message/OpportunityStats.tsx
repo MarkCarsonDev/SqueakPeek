5b1ea78e0d84354d0d5ba2da3b70fbc34302e74c
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-12-03 23:42:31 -0800

    added database types instead of manually adding types for data, added placeholder UI for each stage for loading, error, no apply, and data

--- client/src/ui/message/OpportunityStats.tsx
+++ client/src/ui/message/OpportunityStats.tsx
@@ -3,49 +3,34 @@ import React, { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import { PieChart, pieArcLabelClasses } from "@mui/x-charts";
 import { FetchOpportunityStats } from "@/lib/utils/Application/FetchOpportunityStats";
+import { Database } from "@/lib/types/database.types";
 /**
  * Shows stats based on the opportunity
  */
-export function OpportunityStats(
-  { opportunity_id }: { opportunity_id: string }
-) {
+export function OpportunityStats({
+  opportunity_id,
+}: {
+  opportunity_id: string;
+}) {
   const [data, setData] = useState<
-    | {
-        applied: number | null;
-        created_at: string;
-        interviewing: number | null;
-        month: number | null;
-        offer: number | null;
-        online_assessment: number | null;
-        opportunity_id: string;
-        rejected: number | null;
-        total_applied: number | null;
-        tracking_id: number;
-      }[]
-    | null
-  >(null);
+    Database["public"]["Tables"]["opportunity_tracking"]["Row"][]
+  >([]);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const { data, error } = await FetchOpportunityStats( opportunity_id ); 
+      setIsLoading(true);
+      const { data, error } = await FetchOpportunityStats(opportunity_id);
       if (error) {
-        console.error("Error fetching opportunity stats:", error);
         setError(error);
-        return;
-      }
-      setData(data);
+      } else if (data) setData(data);
+
+      setIsLoading(false);
     }
     fetchData();
   }, []);
 
-  if (error) {
-    return <div>Error loading stats</div>;
-  }
-
-  if (!data) {
-    return <div>No data available</div>;
-  }
   // Combine the total numbers
   const totals = data.reduce(
     (acc, curr) => {
@@ -57,10 +42,17 @@ export function OpportunityStats(
       acc.total_applied += curr.total_applied ?? 0;
       return acc;
     },
-    { applied: 0, rejected: 0, online_assessment: 0, interviewing: 0, offer: 0, total_applied: 0 }
+    {
+      applied: 0,
+      rejected: 0,
+      online_assessment: 0,
+      interviewing: 0,
+      offer: 0,
+      total_applied: 0,
+    }
   );
 
-  const mockData = [
+  const graphData = [
     { id: 0, value: totals.applied, label: "Applied", color: "#779fcd" },
     { id: 1, value: totals.rejected, label: "Rejected", color: "#c7253e" },
     { id: 2, value: totals.online_assessment, label: "OA", color: "#eb5a02" },
@@ -72,41 +64,50 @@ export function OpportunityStats(
     },
     { id: 4, value: totals.offer, label: "Offered", color: "#2e7e33" },
   ];
-  return (
-    <div
-      style={{
-        borderLeft: "3px solid #E0E4F2",
-      }}
-    >
-      <Typography
+
+  if (isLoading) {
+    return <div>Is Loading...</div>;
+  } else if (error || !data) {
+    return <div>Data unavailable</div>;
+  } else if (data.length === 0) {
+    return <div>No One has applied yet</div>;
+  } else {
+    return (
+      <div
         style={{
-          padding: "30px 0px 30px 30px",
+          borderLeft: "3px solid #E0E4F2",
         }}
-        variant="h3"
       >
-        Stats
-      </Typography>
-      <PieChart
-        // TODO: Adjust the data with real data, based on the opportunityID
-        series={[
-          {
-            arcLabel: (item) => `${item.value}`,
-            arcLabelMinAngle: 35,
-            data: mockData,
-          },
-        ]}
-        sx={{
-          display: "flex",
-          fontFamily: "", // for some reason fontFamily is default to something else. This make font change to inter
-          // TODO Figure out how to fix the font for hover tooltip
-          [`& .${pieArcLabelClasses.root}`]: {
-            fontWeight: "bold",
-            fill: "white",
-            color: "white",
-          },
-        }}
-        height={175}
-      />
-    </div>
-  );
+        <Typography
+          style={{
+            padding: "30px 0px 30px 30px",
+          }}
+          variant="h3"
+        >
+          Stats
+        </Typography>
+        <PieChart
+          // TODO: Adjust the data with real data, based on the opportunityID
+          series={[
+            {
+              arcLabel: (item) => `${item.value}`,
+              arcLabelMinAngle: 35,
+              data: graphData,
+            },
+          ]}
+          sx={{
+            display: "flex",
+            fontFamily: "", // for some reason fontFamily is default to something else. This make font change to inter
+            // TODO Figure out how to fix the font for hover tooltip
+            [`& .${pieArcLabelClasses.root}`]: {
+              fontWeight: "bold",
+              fill: "white",
+              color: "white",
+            },
+          }}
+          height={175}
+        />
+      </div>
+    );
+  }
 }
