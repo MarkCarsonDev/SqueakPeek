7ecc04cc594bc5e7ef6d7afb6583e7dc4e97cfa6
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-12-08 15:55:15 -0800

    removed uncesary comments

--- client/src/app/(main)/message/company/[conversationId]/page.tsx
+++ client/src/app/(main)/message/company/[conversationId]/page.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import { OpportunityStats } from "@/ui/message/OpportunityStats";
 import { Conversation } from "@/ui/message/Conversation";
 
-// export const metadata = {
-//   title: "Company Conversation - Chat with Employers",
-//   description:
-//     "Engage in private conversations with employers, discuss opportunities, and share insights.",
-// };
-
 export default function Page({
   params,
 }: {
@@ -23,8 +17,7 @@ export default function Page({
       }}
     >
       <Conversation conversationId={conversationId} />
-      {/* Pass a random opportunity_id */}
-      <OpportunityStats conversation_id={conversationId}/>
+      <OpportunityStats conversation_id={conversationId} />
     </div>
   );
 }
