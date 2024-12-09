ce5933b4c4dc624d897e1c4cfb46ffa831f41c5d
Noah Daniels <noahjdaniels@gmail.com>
2024-12-08 19:51:02 -0800

    not sure what this deletion is

--- client/src/ui/explore/OpportunityCardSkeletonList.tsx
+++
@@ -1,4 +0,0 @@
-import { OpportunityCardSkeleton } from "./OpportunityCardSkeleton";
-export function OpportunityCardSkeletonList() {
-  return [1, 2, 3].map((val) => <OpportunityCardSkeleton key={val} />);
-}
